9ce727e237ff9ea653ba05f5d193dec6f1057a88
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-07-23 06:43:32 -0400

    cv 	new file:   cv/cv.html 	new file:   cv/cv.pdf 	new file:   cv/cv.rmd 	new file:   cv/cv_printing_functions.r 	new file:   cv/data/contact_info.csv 	new file:   cv/data/entries.csv 	new file:   cv/data/language_skills.csv 	new file:   cv/data/text_blocks.csv 	new file:   cv/dd_cv.css 	new file:   cv/render_cv.r 	new file:   cv/setup.r

---
+++ cv/setup.r
@@ -0,0 +1,11 @@
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Daniel Gonzalez",
+  data_location = "./data/",
+  pdf_location = "pdf_location",
+  html_location = "html_location",
+  source_location = "source_location"
+  #pdf_location = "https://github.com/daniel-gonzalez-cedre/cv/raw/master/strayer_cv.pdf",
+  #html_location = "daniel-gonzalez-cedre.github.io/cv/",
+  #source_location = "https://github.com/daniel-gonzalez-cedre/cv"
+)
