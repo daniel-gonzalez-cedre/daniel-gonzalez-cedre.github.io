6068b74f54a16ac8f3f97225542f8dda2e8d8ec0
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2020-07-25 03:29:38 -0400

    wew 	modified:   cv.html 	modified:   cv.pdf 	modified:   cv_printing_functions.r 	modified:   data/contact_info.csv 	modified:   data/entries.csv 	modified:   data/text_blocks.csv

--- cv/cv_printing_functions.r
+++ cv/cv_printing_functions.r
@@ -155,7 +155,8 @@ print_section <- function(cv, section_id, glue_template = "default"){
   # so links for the same position are right next to each other in number.
   for(i in 1:nrow(section_data)){
     for(col in c('title', 'description_bullets')){
-      strip_res <- sanitize_links(cv, section_data[i, col])
+      #strip_res <- sanitize_links(cv, section_data[i, col])
+      strip_res <- list(cv = cv, text = section_data[i, col])
       section_data[i, col] <- strip_res$text
       cv <- strip_res$cv
     }
@@ -174,7 +175,8 @@ print_text_block <- function(cv, label){
   text_block <- dplyr::filter(cv$text_blocks, loc == label) %>%
     dplyr::pull(text)
 
-  strip_res <- sanitize_links(cv, text_block)
+  #strip_res <- sanitize_links(cv, text_block)
+  strip_res <- list(cv = cv, text = text_block)
 
   cat(strip_res$text)
 
