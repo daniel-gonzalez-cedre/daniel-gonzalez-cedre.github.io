ebb945d2ec6a0ae60a3157e07e16a7f5a2d10c02
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2024-01-18 15:21:55 -0500

    Changes to be committed:         modified:   2024spring/LaTeX.zip         modified:   2024spring/syllabus.pdf         deleted:    2024spring/update.sh         modified:   aboutme.html         modified:   favicon.ico         modified:   favicons/stjerome.ico         modified:   index.html         modified:   research.html         modified:   teaching.html         new file:   update.sh

---
+++ update.sh
@@ -0,0 +1,26 @@
+# 2024 SPRING DISCRETE MATH
+cd 2024spring
+
+  # SYLLABUS PDF
+  cp -v ~/academia/pedagogy/2024-spring-discrete-math/syllabus/syllabus.pdf ./
+
+  # LECTURE NOTE PDF
+  cp -v ~/academia/pedagogy/book-discrete-math/discrete-math.pdf ./lecture-notes.pdf
+
+  # PROBLEM SET PDFs & TeX FILES
+  cp -v ~/academia/pedagogy/2024-spring-discrete-math/problem-sets/problem-set-*/problem-set-*.pdf ./problem-sets/
+  cp -v ~/academia/pedagogy/2024-spring-discrete-math/problem-sets/problem-set-*/problem-set-*.tex ./tex/
+
+  # LaTeX PREAMBLES
+  cp -vr ~/LaTeX/preambles ./LaTeX/
+  cp -v ~/LaTeX/templates/tufte_problem-set/config.tex ./LaTeX/
+  rm -v ./LaTeX/preambles/backup_*.tex
+
+  zip -vr LaTeX.zip ./LaTeX -X
+  rm -rf LaTeX
+
+cd ..
+
+# GENERAL WEBSITE
+python3 ./update.py
+git commit -a
