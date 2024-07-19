6d61596b9bcba9739cfbc16fddc7a47ea1bab580
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2024-07-19 13:15:04 -0400

    Changes to be committed:         modified:   aboutme.html         modified:   cv/daniel-gonzalez-cedre-cv.pdf         modified:   index.html         new file:   papers/2023_slides_a_transformational_approach_to_graph_learning.pdf         new file:   papers/2023_slides_explaining_anomalies_in_graphs_with_grammars.pdf         modified:   research.html         modified:   style.css         modified:   teaching.html         modified:   update.sh

--- update.sh
+++ update.sh
@@ -2,31 +2,31 @@
 cd 2024spring
 
   # SYLLABUS PDF
-  cp -v ~/academia/pedagogy/2024-spring-discrete-math/syllabus/syllabus.pdf ./
+    cp -v ~/academia/pedagogy/2024-spring-discrete-math/syllabus/syllabus.pdf ./
 
   # LECTURE NOTE PDF
-  cp -v ~/academia/pedagogy/book-discrete-math/discrete-math.pdf ./lecture-notes.pdf
+    cp -v ~/academia/pedagogy/book-discrete-math/discrete-math.pdf ./lecture-notes.pdf
 
   # PROBLEM SET PDFs & TeX FILES
-  cp -v ~/academia/pedagogy/2024-spring-discrete-math/problem-sets/problem-set-*/problem-set-*.pdf ./problem-sets/
-  cp -v ~/academia/pedagogy/2024-spring-discrete-math/problem-sets/problem-set-*/problem-set-*.tex ./tex/
-  cp -v ~/academia/pedagogy/2024-spring-discrete-math/exams/exam-*.p.*/exam-*.p.*.pdf ./exams/
+    cp -v ~/academia/pedagogy/2024-spring-discrete-math/problem-sets/problem-set-*/problem-set-*.pdf ./problem-sets/
+    cp -v ~/academia/pedagogy/2024-spring-discrete-math/problem-sets/problem-set-*/problem-set-*.tex ./tex/
+    cp -v ~/academia/pedagogy/2024-spring-discrete-math/exams/exam-*.p.*/exam-*.p.*.pdf ./exams/
 
   # LaTeX PREAMBLES
-  mkdir -v ./LaTeX
-  mkdir -v ./LaTeX/preambles
-  cp -vr ~/LaTeX/preambles ./LaTeX/
-  cp -v ~/LaTeX/config_students.tex ./LaTeX/config.tex
-  rm -vr ./LaTeX/preambles/backup
+    # mkdir -v ./LaTeX
+    # mkdir -v ./LaTeX/preambles
+    # cp -vr ~/LaTeX/preambles ./LaTeX/
+    # cp -v ~/LaTeX/config_students.tex ./LaTeX/config.tex
+    # rm -vr ./LaTeX/preambles/backup
 
-  rm -v ./LaTeX.zip
-  zip -vr LaTeX.zip ./LaTeX -X
-  rm -rf LaTeX
+  # LaTeX CLEANUP
+    # rm -v ./LaTeX.zip
+    # zip -vr LaTeX.zip ./LaTeX -X
+    # rm -rf LaTeX
 
 cd ..
 
 # GENERAL WEBSITE
 python3 ./update.py
 
-# git add ./spring2024/problem-sets
 # git commit -a
