f68e15826cfb530168e6f6507c2ce64d49506e9c
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2024-07-28 09:04:21 -0400

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -3,18 +3,18 @@ cd teaching
   # PRINCIPLES OF COMPUTING
 
   # DISCRETE MATH
-  cd 2024spring
-    # SYLLABUS PDF
-      cp -v ~/academia/pedagogy/2024_spring_discrete-math/syllabus/syllabus.pdf ./
+  # cd 2024spring
+    # # SYLLABUS PDF
+      # cp -v ~/academia/pedagogy/2024_spring_discrete-math/syllabus/syllabus.pdf ./
 
-    # LECTURE NOTE PDF
-      cp -v ~/academia/pedagogy/book_discrete-math/discrete-math.pdf ./lecture-notes.pdf
+    # # LECTURE NOTE PDF
+      # cp -v ~/academia/pedagogy/book_discrete-math/discrete-math.pdf ./lecture-notes.pdf
 
-    # PROBLEM SET PDFs & TeX FILES
-      cp -v ~/academia/pedagogy/2024_spring_discrete-math/problem-sets/problem-set-*/problem-set-*.pdf ./problem-sets/
-      cp -v ~/academia/pedagogy/2024_spring_discrete-math/problem-sets/problem-set-*/problem-set-*.tex ./tex/
-      cp -v ~/academia/pedagogy/2024_spring_discrete-math/exams/exam-*.p.*/exam-*.p.*.pdf ./exams/
-  cd ..
+    # # PROBLEM SET PDFs & TeX FILES
+      # cp -v ~/academia/pedagogy/2024_spring_discrete-math/problem-sets/problem-set-*/problem-set-*.pdf ./problem-sets/
+      # cp -v ~/academia/pedagogy/2024_spring_discrete-math/problem-sets/problem-set-*/problem-set-*.tex ./tex/
+      # cp -v ~/academia/pedagogy/2024_spring_discrete-math/exams/exam-*.p.*/exam-*.p.*.pdf ./exams/
+  # cd ..
 
   # LaTeX PREAMBLES
     # mkdir -v ./LaTeX
