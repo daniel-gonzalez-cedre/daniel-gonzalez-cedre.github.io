5223ec9782d2d34f403d24f177febc00ae1fcc7c
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2025-01-11 00:31:11 -0500

    [refactor]
    Changes to be committed:
            deleted:    about-me.html
            new file:   about.html
            renamed:    papers/2015_poster_gonzalez-cedre_monotone_catenary_degree.pdf -> files/2015_poster_gonzalez-cedre_monotone_catenary_degree.pdf
            renamed:    papers/2020_abstract_sikdar_infinity_mirror.pdf -> files/2020_abstract_sikdar_infinity_mirror.pdf
            renamed:    papers/2020_poster_sikdar_infinity_mirror.pdf -> files/2020_poster_sikdar_infinity_mirror.pdf
            renamed:    papers/2020_slides_sikdar_infinity_mirror.pdf -> files/2020_slides_sikdar_infinity_mirror.pdf
            renamed:    papers/2021_paper_hibshman_joint_subgraph-to-subgraph_transitions.pdf -> files/2021_paper_hibshman_joint_subgraph-to-subgraph_transitions.pdf
            renamed:    papers/2022_paper_sikdar_infinity_mirror.pdf -> files/2022_paper_sikdar_infinity_mirror.pdf
            renamed:    papers/2023_paper_theisen_motif_mining.pdf -> files/2023_paper_theisen_motif_mining.pdf
            renamed:    papers/2023_paper_theisen_motif_mining_supplementary.pdf -> files/2023_paper_theisen_motif_mining_supplementary.pdf
            renamed:    papers/2023_poster_theisen_motif_mining.pdf -> files/2023_poster_theisen_motif_mining.pdf
            renamed:    papers/2023_slides_a_transformational_approach_to_graph_learning.pdf -> files/2023_slides_a_transformational_approach_to_graph_learning.pdf
            renamed:    papers/2023_slides_explaining_anomalies_in_graphs_with_grammars.pdf -> files/2023_slides_explaining_anomalies_in_graphs_with_grammars.pdf
            renamed:    papers/2024_poster_carmichael_this_probably_looks_exactly_like_that.pdf -> files/2024_poster_carmichael_this_probably_looks_exactly_like_that.pdf
            new file:   fonts/BerkeleyMono-Bold.woff
            new file:   fonts/BerkeleyMono-Bold.woff2
            new file:   fonts/BerkeleyMono-BoldItalic.woff
            new file:   fonts/BerkeleyMono-BoldItalic.woff2
            new file:   fonts/BerkeleyMono-Italic.woff
            new file:   fonts/BerkeleyMono-Italic.woff2
            new file:   fonts/BerkeleyMonoLigatures-Bold.woff
            new file:   fonts/BerkeleyMonoLigatures-Bold.woff2
            new file:   fonts/BerkeleyMonoLigatures-BoldItalic.woff
            new file:   fonts/BerkeleyMonoLigatures-BoldItalic.woff2
            new file:   fonts/BerkeleyMonoLigatures-Italic.woff
            new file:   fonts/BerkeleyMonoLigatures-Italic.woff2
            new file:   fonts/BerkeleyMonoLigatures-Regular.woff
            new file:   fonts/BerkeleyMonoLigatures-Regular.woff2
            modified:   index.html
            new file:   papers.html
            deleted:    research.html
            new file:   root.css
            new file:   script.js
            new file:   styles/a.css
            new file:   styles/body.css
            new file:   styles/content.css
            new file:   styles/graphics.css
            new file:   styles/lists.css
            new file:   styles/menu.css
            new file:   styles/name.css
            new file:   styles/sidebar.css
            new file:   styles/table.css
            new file:   styles/update.css
            new file:   talks.html
            deleted:    teaching.html
            new file:   teaching/2024spring/exams/exam-1.p.2024.spring.pdf
            new file:   teaching/2024spring/exams/exam-f.p.2024.spring.pdf
            new file:   teaching/discrete_mathematics.html

---
+++ script.js
@@ -0,0 +1,68 @@
+let init = document.querySelectorAll("#autofocus")
+let divs = document.querySelectorAll("a, label");
+let index = Array.prototype.indexOf.call(divs, init[0]);
+
+// function initSelection() {
+  // init.forEach((div, idx) => {
+    // init.classList.toggle("selected", idx === 0);
+    // if (div === init[0]) {
+      // div.focus();
+      // index = idx;
+    // }
+  // });
+  // init[0].focus();
+// }
+
+function updateSelection() {
+  divs.forEach((div, idx) => { div.classList.toggle("selected", idx === index) });
+  divs[index].focus();
+}
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "o":
+    case "O":
+    case "Enter":
+    case " ":  // execute selection
+      event.preventDefault();
+      divs[index].click();  // trigger a mouse click event
+      break;
+    case "p":
+    case "k":
+    case "K":
+    case "ArrowUp":  // select previous element
+      event.preventDefault();
+      index = (index - 1 + divs.length) % divs.length;  // wrap to end
+      updateSelection();
+      break;
+    case "n":
+    case "j":
+    case "J":
+    case "ArrowDown":  // select next element
+      event.preventDefault();
+      index = (index + 1) % divs.length;  // wrap to beginning
+      updateSelection();
+      break;
+    case "Tab":
+      event.preventDefault();
+      if (event.shiftKey) {
+        index = (index - 1 + divs.length) % divs.length;
+        updateSelection();
+      } else {
+        index = (index + 1) % divs.length;
+        updateSelection();
+      }
+      break;
+  }
+});
+
+document.addEventListener("mouseover", (event) => {
+  if (event.target.tagName === "A" || event.target.tagName === "LABEL" ) {
+    divs.forEach((div, idx) => { if (event.target === div) { index = idx; } })
+    updateSelection();
+  }
+})
+
+// initial selection
+// initSelection();
+updateSelection();
